459f124447df3aa59ae0d95da866699c5d67d88f
Iacopo Spalletti <i.spalletti@nephila.it>
2014-08-16 14:05:03 +0200

    Recreate migrations

--- djangocms_video/migrations_django/0001_initial.py
+++ djangocms_video/migrations_django/0001_initial.py
@@ -8,31 +8,31 @@ import cms.models.pluginmodel
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms', '__first__'),
+        ('cms', '0001_initial'),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Video',
             fields=[
-                ('movie', models.FileField(help_text='use .flv file or h264 encoded video file', upload_to=cms.models.pluginmodel.get_plugin_media_path, null=True, verbose_name='movie file', blank=True)),
-                ('movie_url', models.CharField(help_text='vimeo or youtube video url. Example: http://www.youtube.com/watch?v=-iJ7bs4mTUY', max_length=255, null=True, verbose_name='movie url', blank=True)),
-                ('image', models.ImageField(help_text='preview image file', upload_to=cms.models.pluginmodel.get_plugin_media_path, null=True, verbose_name='image', blank=True)),
+                ('cmsplugin_ptr', models.OneToOneField(serialize=False, parent_link=True, auto_created=True, to='cms.CMSPlugin', primary_key=True)),
+                ('movie', models.FileField(help_text='use .flv file or h264 encoded video file', blank=True, null=True, upload_to=cms.models.pluginmodel.get_plugin_media_path, verbose_name='movie file')),
+                ('movie_url', models.CharField(help_text='vimeo or youtube video url. Example: http://www.youtube.com/watch?v=-iJ7bs4mTUY', blank=True, null=True, max_length=255, verbose_name='movie url')),
+                ('image', models.ImageField(help_text='preview image file', blank=True, null=True, upload_to=cms.models.pluginmodel.get_plugin_media_path, verbose_name='image')),
                 ('width', models.PositiveSmallIntegerField(verbose_name='width')),
                 ('height', models.PositiveSmallIntegerField(verbose_name='height')),
-                ('auto_play', models.BooleanField(default=False, verbose_name='auto play')),
-                ('auto_hide', models.BooleanField(default=False, verbose_name='auto hide')),
-                ('fullscreen', models.BooleanField(default=True, verbose_name='fullscreen')),
-                ('loop', models.BooleanField(default=False, verbose_name='loop')),
-                ('bgcolor', models.CharField(default=b'000000', help_text='Hexadecimal, eg ff00cc', max_length=6, verbose_name='background color')),
-                ('textcolor', models.CharField(default=b'FFFFFF', help_text='Hexadecimal, eg ff00cc', max_length=6, verbose_name='text color')),
-                ('seekbarcolor', models.CharField(default=b'13ABEC', help_text='Hexadecimal, eg ff00cc', max_length=6, verbose_name='seekbar color')),
-                ('seekbarbgcolor', models.CharField(default=b'333333', help_text='Hexadecimal, eg ff00cc', max_length=6, verbose_name='seekbar bg color')),
-                ('loadingbarcolor', models.CharField(default=b'828282', help_text='Hexadecimal, eg ff00cc', max_length=6, verbose_name='loadingbar color')),
-                ('buttonoutcolor', models.CharField(default=b'333333', help_text='Hexadecimal, eg ff00cc', max_length=6, verbose_name='button out color')),
-                ('buttonovercolor', models.CharField(default=b'000000', help_text='Hexadecimal, eg ff00cc', max_length=6, verbose_name='button over color')),
-                ('buttonhighlightcolor', models.CharField(default=b'FFFFFF', help_text='Hexadecimal, eg ff00cc', max_length=6, verbose_name='button highlight color')),
-                ('cmsplugin_ptr', models.OneToOneField(auto_created=True, primary_key=True, serialize=False, to='cms.CMSPlugin')),
+                ('auto_play', models.BooleanField(verbose_name='auto play', default=False)),
+                ('auto_hide', models.BooleanField(verbose_name='auto hide', default=False)),
+                ('fullscreen', models.BooleanField(verbose_name='fullscreen', default=True)),
+                ('loop', models.BooleanField(verbose_name='loop', default=False)),
+                ('bgcolor', models.CharField(help_text='Hexadecimal, eg ff00cc', default='000000', max_length=6, verbose_name='background color')),
+                ('textcolor', models.CharField(help_text='Hexadecimal, eg ff00cc', default='FFFFFF', max_length=6, verbose_name='text color')),
+                ('seekbarcolor', models.CharField(help_text='Hexadecimal, eg ff00cc', default='13ABEC', max_length=6, verbose_name='seekbar color')),
+                ('seekbarbgcolor', models.CharField(help_text='Hexadecimal, eg ff00cc', default='333333', max_length=6, verbose_name='seekbar bg color')),
+                ('loadingbarcolor', models.CharField(help_text='Hexadecimal, eg ff00cc', default='828282', max_length=6, verbose_name='loadingbar color')),
+                ('buttonoutcolor', models.CharField(help_text='Hexadecimal, eg ff00cc', default='333333', max_length=6, verbose_name='button out color')),
+                ('buttonovercolor', models.CharField(help_text='Hexadecimal, eg ff00cc', default='000000', max_length=6, verbose_name='button over color')),
+                ('buttonhighlightcolor', models.CharField(help_text='Hexadecimal, eg ff00cc', default='FFFFFF', max_length=6, verbose_name='button highlight color')),
             ],
             options={
                 'abstract': False,
