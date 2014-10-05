caca16c2da3efa1c2f6dec815c309c155fa2a3c9
John-Scott Atlakson <john.scott.atlakson@gmail.com>
2014-10-05 13:29:22 -0400

    Fixed migrations to use settings for defaults

--- djangocms_video/migrations_django/0001_initial.py
+++ djangocms_video/migrations_django/0001_initial.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models, migrations
 import cms.models.pluginmodel
+from djangocms_video import settings
 
 
 class Migration(migrations.Migration):
@@ -21,18 +22,18 @@ class Migration(migrations.Migration):
                 ('image', models.ImageField(null=True, verbose_name='image', upload_to=cms.models.pluginmodel.get_plugin_media_path, help_text='preview image file', blank=True)),
                 ('width', models.PositiveSmallIntegerField(verbose_name='width')),
                 ('height', models.PositiveSmallIntegerField(verbose_name='height')),
-                ('auto_play', models.BooleanField(default=False, verbose_name='auto play')),
-                ('auto_hide', models.BooleanField(default=False, verbose_name='auto hide')),
-                ('fullscreen', models.BooleanField(default=True, verbose_name='fullscreen')),
-                ('loop', models.BooleanField(default=False, verbose_name='loop')),
-                ('bgcolor', models.CharField(max_length=6, default='000000', help_text='Hexadecimal, eg ff00cc', verbose_name='background color')),
-                ('textcolor', models.CharField(max_length=6, default='FFFFFF', help_text='Hexadecimal, eg ff00cc', verbose_name='text color')),
-                ('seekbarcolor', models.CharField(max_length=6, default='13ABEC', help_text='Hexadecimal, eg ff00cc', verbose_name='seekbar color')),
-                ('seekbarbgcolor', models.CharField(max_length=6, default='333333', help_text='Hexadecimal, eg ff00cc', verbose_name='seekbar bg color')),
-                ('loadingbarcolor', models.CharField(max_length=6, default='828282', help_text='Hexadecimal, eg ff00cc', verbose_name='loadingbar color')),
-                ('buttonoutcolor', models.CharField(max_length=6, default='333333', help_text='Hexadecimal, eg ff00cc', verbose_name='button out color')),
-                ('buttonovercolor', models.CharField(max_length=6, default='000000', help_text='Hexadecimal, eg ff00cc', verbose_name='button over color')),
-                ('buttonhighlightcolor', models.CharField(max_length=6, default='FFFFFF', help_text='Hexadecimal, eg ff00cc', verbose_name='button highlight color')),
+                ('auto_play', models.BooleanField(default=settings.VIDEO_AUTOPLAY, verbose_name='auto play')),
+                ('auto_hide', models.BooleanField(default=settings.VIDEO_AUTOHIDE, verbose_name='auto hide')),
+                ('fullscreen', models.BooleanField(default=settings.VIDEO_FULLSCREEN, verbose_name='fullscreen')),
+                ('loop', models.BooleanField(default=settings.VIDEO_LOOP, verbose_name='loop')),
+                ('bgcolor', models.CharField(max_length=6, default=settings.VIDEO_BG_COLOR, help_text='Hexadecimal, eg ff00cc', verbose_name='background color')),
+                ('textcolor', models.CharField(max_length=6, default=settings.VIDEO_TEXT_COLOR, help_text='Hexadecimal, eg ff00cc', verbose_name='text color')),
+                ('seekbarcolor', models.CharField(max_length=6, default=settings.VIDEO_SEEKBAR_COLOR, help_text='Hexadecimal, eg ff00cc', verbose_name='seekbar color')),
+                ('seekbarbgcolor', models.CharField(max_length=6, default=settings.VIDEO_SEEKBARBG_COLOR, help_text='Hexadecimal, eg ff00cc', verbose_name='seekbar bg color')),
+                ('loadingbarcolor', models.CharField(max_length=6, default=settings.VIDEO_LOADINGBAR_COLOR, help_text='Hexadecimal, eg ff00cc', verbose_name='loadingbar color')),
+                ('buttonoutcolor', models.CharField(max_length=6, default=settings.VIDEO_BUTTON_OUT_COLOR, help_text='Hexadecimal, eg ff00cc', verbose_name='button out color')),
+                ('buttonovercolor', models.CharField(max_length=6, default=settings.VIDEO_BUTTON_OVER_COLOR, help_text='Hexadecimal, eg ff00cc', verbose_name='button over color')),
+                ('buttonhighlightcolor', models.CharField(max_length=6, default=settings.VIDEO_BUTTON_HIGHLIGHT_COLOR, help_text='Hexadecimal, eg ff00cc', verbose_name='button highlight color')),
             ],
             options={
                 'abstract': False,
