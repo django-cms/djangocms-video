35496493e60ce60d60c020e246fa897d74035ff1
Angelo Dini <angelo.dini@divio.ch>
2016-08-11 12:08:24 +0200

    initial file cleanup

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from setuptools import setup
+from setuptools import find_packages, setup
+
 from djangocms_video import __version__
+
+
+REQUIREMENTS = [
+    'django-cms>=3.3.1',
+    'djangocms-attributes-field>=0.1.1',
+]
+
 
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
@@ -11,29 +19,31 @@ CLASSIFIERS = [
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
-    'Topic :: Communications',
-    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-    'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: Message Boards',
-    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Topic :: Internet :: WWW/HTTP',
+    'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    'Topic :: Software Development :: Libraries :: Python Modules',
 ]
+
 
 setup(
     name='djangocms-video',
     version=__version__,
-    description='Video plugin for django CMS',
+    description=('Adds video plugin to django CMS.'),
     author='Divio AG',
-    author_email='info@divio.ch',
+    author_email='info@divio.com',
     url='https://github.com/divio/djangocms-video',
-    packages=[
-        'djangocms_video',
-        'djangocms_video.migrations',
-        'djangocms_video.south_migrations'
-    ],
-    license='LICENSE.txt',
-    platforms=['OS Independent'],
-    classifiers=CLASSIFIERS,
+    license='BSD',
     long_description=open('README.rst').read(),
+    packages=find_packages(),
     include_package_data=True,
-    zip_safe=False
+    zip_safe=False,
+    install_requires=REQUIREMENTS,
+    classifiers=CLASSIFIERS,
+    test_suite='tests.settings.run',
 )
